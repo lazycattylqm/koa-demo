d62c616f8ed33aee6658d444b1476544c7a74a32
lazycattylqm <leeqiming@163.com>
2025-03-03 23:53:05 +0800

    update .gitignore, add dotenv and file handling routes; implement file upload and download functionality

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,43 @@
-import { bodyParser } from '@koa/bodyparser';
 import * as Cors from '@koa/cors';
+import * as dotenv from 'dotenv';
 import * as Koa from 'koa';
+import KoaBody, { HttpMethodEnum } from 'koa-body';
+import * as KoaStatic from 'koa-static';
+import { nanoid } from 'nanoid';
+import * as path from 'path';
 import router from './route';
+
+dotenv.config()
 
 const app = new Koa();
 
 app.use(Cors());
-app.use(bodyParser());
+app.use(KoaBody({
+  multipart: true,
+  formidable: {
+    maxFileSize: 200 * 1024 * 1024, // 200MB'
+    keepExtensions: true,
+    uploadDir: path.join(path.resolve(process.cwd()), 'public/upload'),
+    onFileBegin: (name, file) => { // 文件开始处理钩子
+      // 生成自定义文件名逻辑
+      const ext = path.extname(file.originalFilename);
+      const filename = `${Date.now()}-${nanoid(6)}${ext}`;
+      // 修改最终存储路径
+      file.filepath = path.join(path.resolve(process.cwd()), 'public/upload', filename);
+
+      // 安全过滤（防止路径穿越）
+      file.originalFilename = file.originalFilename.replace(/[^a-zA-Z0-9_\-.]/g, '');
+    }
+  },
+  parsedMethods: [HttpMethodEnum.POST, HttpMethodEnum.PUT, HttpMethodEnum.PATCH, HttpMethodEnum.DELETE, HttpMethodEnum.GET, HttpMethodEnum.HEAD],
+
+
+}));
+
+app.use(KoaStatic(path.join(path.resolve(process.cwd()), 'public')));
+
 app.use(router.router());
 
-app.listen(4000, () => {
-  console.log('Listening to port 4000');
+app.listen(process.env.port, () => {
+  console.log(`Listening to port ${process.env.port}`);
 });
--- src/route/file.route.ts
+++ src/route/file.route.ts
@@ -3,22 +3,44 @@ import * as Router from 'koa-router';
 import * as path from 'path';
 import { FileRequest, FileResponse } from '../type';
 
-const router: Router = new Router();
+const router: Router = new Router({ prefix: '/api' });
 
 const basePath: string = path.join(path.resolve(process.cwd()), 'data');
+if (!fs.existsSync(basePath)) {
+  fs.mkdirSync(basePath);
+}
+
+const uploadPath: string = path.join(path.resolve(process.cwd()), 'public/upload');
+
+if (!fs.existsSync(uploadPath)) {
+  fs.mkdirSync(uploadPath, { recursive: true });
+}
 
 router.put('/file', async (ctx: any) => {
   const body: FileRequest = ctx.request.body as FileRequest
   const fileName: string = body.fileName
-  if (!fs.existsSync(basePath)) {
-    fs.mkdirSync(basePath);
-  }
+
 
   fs.writeFileSync(path.join(basePath, fileName), JSON.stringify(body.items), { flag: 'w' });
   const response: FileResponse = {
     message: 'File created!'
   }
   ctx.body = response;
+});
+
+router.post('/upload', async (ctx: any) => {
+  const { filepath } = ctx.request.files.file;
+  const response: FileResponse = {
+    message: `${path.basename(filepath)} File uploaded!`
+  }
+  ctx.body = response;
+});
+
+router.get('/download/:file', async (ctx: any) => {
+  const file = ctx.params.file;
+  const filePath = path.join(uploadPath, file);
+  ctx.attachment(filePath);
+  ctx.body = fs.createReadStream(filePath);
 });
 
 export default router;
